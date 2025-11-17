e16160ef6f5c8b4c8f68be82c99a6608b468c840
2395959141 <2395959141@qq.com>
2025-11-18 00:38:34 +0800

    feat: add MMA PTX wrapper example

---
+++ hand_on_flash_attn2/src/include/ptx_functions.cuh
@@ -0,0 +1,45 @@
+
+#pragma once
+
+#include <cuda_bf16.h>
+#include <cuda_fp16.h>
+#include <cstdint>
+#include <type_traits>
+
+
+template <typename value_t>
+void mma_m16n8k16_f32_accum (
+    float &d1, float &d2, float &d3, float &d4,
+
+    uint32_t const &a1, uint32_t const &a2,
+    uint32_t const &a3, uint32_t const &a4,
+
+    uint32_t const &b1, uint32_t const &b2,
+
+    float const &c1, float const &c2,
+    float const &c3, float const &c4
+) {
+    static_assert(std::is_same_v<value_t, __half> ||
+                    std::is_same_v<value_t, __nv_bfloat16>,
+                 "value_t must be either __half or __nv_bfloat16");
+    
+    if constexpr (std::is_same_v<value_t, __nv_bfloat16>) {
+        asm volatile ("mma.sync.aligned.m16n8k16.row.col.f32.bf16.bf16.f32"
+                      " { %0, %1, %2, %3},"
+                      " { %4, %5, %6, %7},"
+                      " { %8, %9},"
+                      " { %10, %11, %12, %13}; "
+                      : "=f"(d1), "=f"(d2), "=f"(d3), "=f"(d4)
+                      : "r"(a1), "r"(a2), "r"(a3), "r"(a4), "r"(b1), "r"(b2),
+                        "f"(c1), "f"(c2), "f"(c3), "f"(c4));
+    } else {
+        asm volatile ("mma.sync.aligned.m16n8k16.row.col.f32.f16.f16.f32"
+                      " { %0, %1, %2, %3},"
+                      " { %4, %5, %6, %7},"
+                      " { %8, %9},"
+                      " { %10, %11, %12, %13}; "
+                      : "=f"(d1), "=f"(d2), "=f"(d3), "=f"(d4)
+                      : "r"(a1), "r"(a2), "r"(a3), "r"(a4), "r"(b1), "r"(b2),
+                        "f"(c1), "f"(c2), "f"(c3), "f"(c4));
+    }
+}
